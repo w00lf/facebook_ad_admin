0f665ff3e489ba3c8b2df0667b4cf0c3d8bbe29a
Тараскин Михаил <MITaraskin@domclick.ru>
2018-09-12 18:55:10 +0300

    Some tweaks

--- app/admin/facebook_accounts.rb
+++ app/admin/facebook_accounts.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register FacebookAccount do
   permit_params :active, :name, :api_identificator, :api_token, :api_secret, :facebook_group_account_id
+
+  index do
+    id_column
+    column :active
+    column :name
+    column 'Account_id', :api_identificator
+    column :api_token do |obj|
+      truncate(obj.api_token)
+    end
+    column :api_secret
+    column :facebook_group_account
+    actions
+  end
 end
