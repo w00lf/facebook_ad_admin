44ffec107a9cbed64ed5e35711f430e937e9e3b3
Тараскин Михаил <MITaraskin@domclick.ru>
2019-11-26 07:55:50 +0300

    Fix to hide all comments from all adcreatives and without filter

--- app/services/facebook_api/hide_comments_post_service.rb
+++ app/services/facebook_api/hide_comments_post_service.rb
@@ -4,20 +4,22 @@ module FacebookApi
       ad_account = ::FacebookAccountApiRepresentation.new(facebook_account: facebook_account, date: Time.now, logger: Logger.new(STDOUT))
       account = with_exception_control { FacebookAds::User.get('me', ad_account.session).accounts.first }
       page_access_token = account.access_token
-      page_post_id = with_exception_control { ad_account.adcreatives(fields: %i[id effective_object_story_id]).first.effective_object_story_id }
-      page_post = with_exception_control { FacebookAds::PagePost.get(page_post_id, ad_account.session) }
-      comments = with_exception_control { page_post.comments.to_a }
-      batch = FacebookAds::Batch.with_batch do
-        comments.each.with_index(1) do |comment, i|
-          break if i > max_comments
+      ad_account.adcreatives(fields: %i[id effective_object_story_id]).each do |adcreative|
+        page_post_id = with_exception_control { adcreative.effective_object_story_id }
+        page_post = with_exception_control { FacebookAds::PagePost.get(page_post_id, ad_account.session) }
+        comments = with_exception_control { page_post.comments(filter: 'stream').to_a }
+        batch = FacebookAds::Batch.with_batch do
+          comments.each.with_index(1) do |comment, i|
+            break if i > max_comments
 
-          puts(comment.message, comment.id)
-          comment_object = with_exception_control { FacebookAds::Comment.get(comment.id, access_token: page_access_token) }
-          comment_object.is_hidden = true
-          comment_object.save
+            puts(comment.message, comment.id)
+            comment_object = with_exception_control { FacebookAds::Comment.get(comment.id, access_token: page_access_token) }
+            comment_object.is_hidden = true
+            comment_object.save
+          end
         end
+        with_exception_control { batch.execute }
       end
-      with_exception_control { batch.execute }
     end
   end
 end
