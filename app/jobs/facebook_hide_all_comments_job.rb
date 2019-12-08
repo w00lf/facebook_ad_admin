e5c32bdce263f26836204eb0bb3070fa0e7e8e51
Тараскин Михаил <MITaraskin@domclick.ru>
2019-12-08 10:07:26 +0300

    Lower schedule time - hide all comments

--- app/jobs/facebook_hide_all_comments_job.rb
+++ app/jobs/facebook_hide_all_comments_job.rb
@@ -6,7 +6,7 @@ class FacebookHideAllCommentsJob < ApplicationJob
     logger.info('Started hidding all comment')
     index = 0
     FacebookAccount.joins(:facebook_group_account).where(active: true, facebook_group_accounts: { active: true }).find_each do |facebook_account|
-      FacebookAccountHideCommentsJob.set(wait: (index * 120).seconds).perform_later(facebook_account.id)
+      FacebookAccountHideCommentsJob.set(wait: (index * 30).seconds).perform_later(facebook_account.id)
       index += 1
       logger.info("Setted FacebookAccountHideCommentsJob for #{facebook_account.id}")
     end
