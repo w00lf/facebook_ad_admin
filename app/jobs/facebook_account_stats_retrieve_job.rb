22d40d1ae8e870688fbdf67ca46fca143bedf217
Тараскин Михаил <MITaraskin@domclick.ru>
2020-11-29 12:25:20 +0300

    Removed byebug debugger mark

--- app/jobs/facebook_account_stats_retrieve_job.rb
+++ app/jobs/facebook_account_stats_retrieve_job.rb
@@ -59,7 +59,6 @@ class FacebookAccountStatsRetrieveJob < ApplicationJob
     end
     parse_result.update!(status: 'ok')
   rescue => e
-    byebug
     parse_result.update!(status: 'error', error_type: e.class, error_text: e.message)
   ensure
     log_file.close
